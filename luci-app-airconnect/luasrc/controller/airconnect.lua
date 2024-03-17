5af61b8234d4bfec43ccc780aea3a658776bf1a6
action <actions@github.com>
2024-03-17 16:21:09 +0800

    update 2024-03-17 16:21:09

--- luci-app-airconnect/luasrc/controller/airconnect.lua
+++ luci-app-airconnect/luasrc/controller/airconnect.lua
@@ -1,20 +1,7 @@
+
 module("luci.controller.airconnect", package.seeall)
 
 function index()
-	if not nixio.fs.access("/etc/config/airconnect") then
-		return
-	end
-
-	local page = entry({"admin", "services", "airconnect"}, cbi("airconnect"), _("AirConnect"))
-	page.dependent = true
-	page.acl_depends = { "luci-app-airconnect" }
-
-	entry({"admin", "services", "airconnect", "status"}, call("act_status")).leaf = true
-end
-
-function act_status()
-	local e = {}
-	e.running = luci.sys.call("pgrep aircast >/dev/null") == 0 or luci.sys.call("pgrep airupnp >/dev/null") == 0
-	luci.http.prepare_content("application/json")
-	luci.http.write_json(e)
+  entry({"admin", "services", "airconnect"}, alias("admin", "services", "airconnect", "config"), _("AirConnect"), 90).dependent = true
+  entry({"admin", "services", "airconnect", "config"}, cbi("airconnect"))
 end
