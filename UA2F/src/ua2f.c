4e52bc305b40307ade99b21b90670ca432492a6b
action <actions@github.com>
2024-07-01 00:25:37 +0800

    update 2024-07-01 00:25:37

--- UA2F/src/ua2f.c
+++ UA2F/src/ua2f.c
@@ -23,6 +23,45 @@ void signal_handler(const int signum) {
     should_exit = true;
 }
 
+int parse_packet(const struct nf_queue *queue, struct nf_buffer *buf) {
+    struct nf_packet packet[1] = {0};
+
+    while (!should_exit) {
+        const __auto_type status = nfqueue_next(buf, packet);
+        switch (status) {
+        case IO_READY:
+            handle_packet(queue, packet);
+            break;
+        default:
+            return status;
+        }
+    }
+
+    return IO_ERROR;
+}
+
+int read_buffer(struct nf_queue *queue, struct nf_buffer *buf) {
+    const __auto_type buf_status = nfqueue_receive(queue, buf, 0);
+    switch (buf_status) {
+    case IO_READY:
+        return parse_packet(queue, buf);
+    default:
+        return buf_status;
+    }
+}
+
+void main_loop(struct nf_queue *queue) {
+    struct nf_buffer buf[1] = {0};
+
+    while (!should_exit) {
+        if (read_buffer(queue, buf) == IO_ERROR) {
+            break;
+        }
+    }
+
+    free(buf->data);
+}
+
 int main(const int argc, char *argv[]) {
     openlog("UA2F", LOG_PID, LOG_SYSLOG);
 
@@ -44,7 +83,6 @@ int main(const int argc, char *argv[]) {
     signal(SIGABRT, signal_handler);
 
     struct nf_queue queue[1] = {0};
-    struct nf_buffer buf[1] = {0};
 
     const __auto_type ret = nfqueue_open(queue, QUEUE_NUM, 0);
     if (!ret) {
@@ -52,26 +90,8 @@ int main(const int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    while (!should_exit) {
-        if (nfqueue_receive(queue, buf, 0) == IO_READY) {
-            while (!should_exit) {
-                struct nf_packet packet[1];
-                switch (nfqueue_next(buf, packet)) {
-                case IO_ERROR:
-                    should_exit = true;
-                case IO_READY:
-                    handle_packet(queue, packet);
-                case IO_NOTREADY:
-                    continue;
-                default:
-                    syslog(LOG_ERR, "Unknown return value [%s:%d]", __FILE__, __LINE__);
-                    should_exit = true;
-                }
-            }
-        }
-    }
+    main_loop(queue);
 
-    free(buf->data);
     nfqueue_close(queue);
 
     return EXIT_SUCCESS;
