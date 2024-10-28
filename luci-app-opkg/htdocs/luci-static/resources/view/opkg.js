2e7f12e86f6c40bb317db29dc1017fc207675cf9
action <actions@github.com>
2024-10-28 20:49:10 +0800

    update 2024-10-28 20:49:10

--- luci-app-opkg/htdocs/luci-static/resources/view/opkg.js
+++ luci-app-opkg/htdocs/luci-static/resources/view/opkg.js
@@ -995,10 +995,7 @@ function handleOpkg(ev)
 				_('Waiting for the <em>opkg %h</em> command to complete…').format(cmd))
 		]);
 
-		var argv = [ cmd ];
-
-		if (cmd == 'remove')
-			argv.push('--force-removal-of-dependent-packages')
+		var argv = [ cmd, '--force-removal-of-dependent-packages' ];
 
 		argv.push('--force-checksum');
 		
