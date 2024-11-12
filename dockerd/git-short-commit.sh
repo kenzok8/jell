cac427840b48abab38daf3f4342b257be4561545
action <actions@github.com>
2024-11-13 00:30:58 +0800

    update 2024-11-13 00:30:58

--- dockerd/git-short-commit.sh
+++ dockerd/git-short-commit.sh
@@ -1,4 +1,3 @@
-
 #!/bin/sh
 #
 #	USAGE: git-short-commit.sh <GIT_URL> <GIT_REF> <GIT_DIR>
@@ -27,7 +26,7 @@ if [ -z "${GIT_DIR}" ]; then
 fi
 
 clean_up() {
-	rm -rf "${GIT_DIR}"
+	rm --force --recursive "${GIT_DIR}"
 }
 trap clean_up EXIT
 
