20b114c1cacfd4a02889cf2d01bed30fc3bdcc8b
action <actions@github.com>
2025-07-23 10:23:50 +0800

    update 2025-07-23 10:23:50

--- luci-app-modem/luasrc/controller/modem.lua
+++ luci-app-modem/luasrc/controller/modem.lua
@@ -953,7 +953,6 @@ function getPluginInfo()
 	usb_driver_info["qmi_wwan_q.ko"]="Not loaded"
 	usb_driver_info["qmi_wwan_f.ko"]="Not loaded"
 	usb_driver_info["qmi_wwan_m.ko"]="Not loaded"
-	usb_driver_info["qmi_wwan_t.ko"]="Not loaded"
 	usb_driver_info["meig_cdc_driver.ko"]="Not loaded"
 	setModelStatus(usb_driver_info)
 
