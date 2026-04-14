04281e580d95b9f4e793fd035b3334371d2aa7b7
action <actions@github.com>
2026-04-14 11:34:52 +0800

    update 2026-04-14 11:34:52

--- luci-app-verysync/luasrc/controller/verysync.lua
+++ luci-app-verysync/luasrc/controller/verysync.lua
@@ -7,12 +7,12 @@ function index()
 	end
 
 	entry({"admin", "nas", "verysync"}, cbi("verysync"), _("Verysync"), 10).dependent = true
-	entry({"admin","nas","verysync","status"},call("act_status")).leaf=true
+	entry({"admin", "nas", "verysync", "status"}, call("act_status")).leaf = true
 end
 
 function act_status()
-	local e={}
-	e.running=luci.sys.call("pgrep verysync >/dev/null")==0
+	local e = {}
+	e.running = luci.sys.call("pgrep verysync >/dev/null") == 0
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(e)
 end
