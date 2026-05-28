c64183fa699a3b7678d91296b69ebc7ee5a2cb80
action <actions@github.com>
2026-05-28 17:35:10 +0800

    update 2026-05-28 17:35:10

--- luci-app-onliner/README.md
+++
@@ -1,9 +0,0 @@
-# luci-app-onliner
-
-## 修改内容
-
-   - 汉化
-
-## Based on
-
-   - [rufengsuixing](https://github.com/rufengsuixing/luci-app-onliner)
--- luci-app-shutdown/luasrc/model/cbi/shutdown.lua
+++
@@ -1,7 +0,0 @@
-m = Map("system", translate("Shutdown / Reboot"),
-	translate("Use the buttons below to reboot or shut down the device."))
-
-s = m:section(SimpleSection)
-s.template = "shutdown/actions"
-
-return m
